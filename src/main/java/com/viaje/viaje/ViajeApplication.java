79aba628aadd0afb204bccc72a60765393fcf279
laira2 <laira241@gmail.com>
2024-07-01 09:31:52 +0900

    first commit

---
+++ src/main/java/com/viaje/viaje/ViajeApplication.java
@@ -0,0 +1,13 @@
+package com.viaje.viaje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViajeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViajeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viaje/viaje/ViajeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viaje.viaje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViajeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
