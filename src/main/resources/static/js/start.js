f985831198a07bace2bba4a008725bba3a272b1e
aiden_Lee <isyouh77@gmail.com>
2024-07-01 12:22:34 +0900

    main page update

--- src/main/resources/static/js/start.js
+++ src/main/resources/static/js/start.js
@@ -4,8 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const slideUpInnerContent = document.getElementById('slide-up-inner-content');
 
     if (bounceButton && slideUpContent && slideUpInnerContent) {
-        bounceButton.addEventListener('click', function() {
-            console.log('Button clicked');
+        bounceButton.addEventListener('click', function(event) {
+            console.log('Start button clicked');
             fetch('/templates/start.html')
                 .then(response => {
                     if (!response.ok) {
@@ -18,6 +18,18 @@ document.addEventListener('DOMContentLoaded', function() {
                     slideUpContent.classList.add('active');
                 })
                 .catch(error => console.error('Error loading start.html:', error));
+            event.stopPropagation(); // 이벤트 버블링 중지
+        });
+
+        document.body.addEventListener('click', function(event) {
+            if (slideUpContent.classList.contains('active')) {
+                console.log('Body clicked');
+                slideUpContent.classList.remove('active');
+            }
+        });
+
+        slideUpContent.addEventListener('click', function(event) {
+            event.stopPropagation(); // 슬라이드 업된 창 클릭 시 이벤트 버블링 중지
         });
     } else {
         console.error('Button, slide-up content, or inner content not found');
