dca6c8e2ccf6259f1c9d3e56de5994f8b2442612
aiden_Lee <isyouh77@gmail.com>
2024-07-02 13:40:48 +0900

    수정이 되지 않았습니다 pull하기위한 commit입니다!

--- src/main/java/com/viaje/viaje/ViajeApplication.java
+++ src/main/java/com/viaje/viaje/ViajeApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+
+
 @SpringBootApplication
 public class ViajeApplication {
 
@@ -12,3 +14,6 @@ public class ViajeApplication {
 	}
 
 }
+
+
+
