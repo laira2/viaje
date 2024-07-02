0af9b5bc03a03c8b6565f0bca8e1496cef5392bb
laira2 <laira241@gmail.com>
2024-07-02 17:11:12 +0900

    updatePlan

--- src/main/java/com/viaje/viaje/repository/TravelPlansRepository.java
+++ src/main/java/com/viaje/viaje/repository/TravelPlansRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface TravelPlansRepository extends JpaRepository<TravelPlans, String> {
+public interface TravelPlansRepository extends JpaRepository<TravelPlans, Long> {
 
     List<TravelPlans> findAllByTitle(String title);
     List<TravelPlans> findAllByUser_userId(Long userId);
--- src/main/java/com/viaje/viaje/service/TravelPlansService.java
+++ src/main/java/com/viaje/viaje/service/TravelPlansService.java
@@ -1,8 +1,13 @@
 package com.viaje.viaje.service;
 
+import com.viaje.viaje.dto.TravelPlansDTO;
 import com.viaje.viaje.model.TravelPlans;
 import com.viaje.viaje.repository.TravelPlansRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class TravelPlansService {
@@ -13,11 +18,39 @@ public class TravelPlansService {
         this.travelPlansRepository = travelPlansRepository;
     }
 
-//    public TravelPlans postPlan(TravelPlans travelPlans){
-//
-//
-//
-//        return travelPlans.getPlanId();
-//
-//    }
+    public void postPlan(TravelPlans travelPlans) {
+        travelPlansRepository.save(travelPlans);
+    }
+
+    public String updateTravelPlan(HttpSession session, Long planId, TravelPlansDTO updatedDTO) {
+        TravelPlans plan = travelPlansRepository.findById(planId)
+                .orElseThrow(() -> new EntityNotFoundException("TravelPlan not found"));
+
+        if (plan.getUser() == session.getAttribute("user")) {
+            plan.setNation(updatedDTO.getNation());
+            plan.setTitle(updatedDTO.getTitle());
+            plan.setDetail(updatedDTO.getDetail());
+            // 다른 필드들도 필요에 따라 업데이트
+            travelPlansRepository.save(plan);
+            return "redirect:/";
+        }
+        else{
+            return "redirect:/plans/new";
+        }
+
+
+    }
+
+    public String updateStatus(HttpSession session, Long planId){
+        Optional<TravelPlans> plan = travelPlansRepository.findById(planId);
+        if (plan.isPresent()){
+            return "redirect:/plans/new";
+        }else{
+            return "redirect:/";
+        }
+
+
+    }
+
+
 }
