052a1c3b730b9007b3b654db3f6417c212ace929
JeongChangYeol <devcy1029@gmail.com>
2024-07-02 14:18:38 +0900

    signup modify security delete

--- src/main/java/com/viaje/viaje/dto/UserDTO.java
+++ src/main/java/com/viaje/viaje/dto/UserDTO.java
@@ -6,16 +6,17 @@ import lombok.*;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class UserDTO {
 
-    private long user_id;  // 사용자 ID
+    private long userId;  // 사용자 ID
     private String uuid;  // UUID는 문자열로 저장됩니다.
-    private String user_name;  // 사용자 이름
+    private String userName;  // 사용자 이름
     private String nickname;  // 닉네임
     private String password;  // 비밀번호
     private String email;  // 이메일 주소
-    private LocalDateTime created_at;  // 생성 일시
-    private LocalDateTime updated_at;  // 업데이트 일시
+    private LocalDateTime createdAt;  // 생성 일시
+    private LocalDateTime updatedAt;  // 업데이트 일시
     private String point;  // 포인트
 
 }
--- src/main/java/com/viaje/viaje/repository/UserRepository.java
+++ src/main/java/com/viaje/viaje/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.viaje.viaje.repository;
 
-import com.viaje.viaje.model.User;
+import com.viaje.viaje.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-    Optional<User> findByUserId(long user_id);
+public interface UserRepository extends JpaRepository<Users, Long> {
+    Optional<Users> findByEmail(String email);
+    boolean existsByEmail(String email);
+    Optional<Users> findByUserId(long userId);
+    boolean existsByUserId(long userId);  // 사용자 ID 존재 여부 확인
 }
