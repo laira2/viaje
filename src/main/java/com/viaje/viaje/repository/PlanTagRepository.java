187c55e1fbbb4d102a2b33dde2b4a67f665eaeb1
laira2 <laira241@gmail.com>
2024-07-03 17:56:37 +0900

    저장

--- src/main/java/com/viaje/viaje/repository/PlanTagRepository.java
+++ src/main/java/com/viaje/viaje/repository/PlanTagRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PlanTagRepository extends JpaRepository<PlanTag, PlanTag.PlanTagId> {
     // 추가적인 쿼리 메소드를 여기에 정의할 수 있습니다.
+
 }
