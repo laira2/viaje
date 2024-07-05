ca5af4dde4d2f2e0e1d596a414df23268c182e4a
laira2 <laira241@gmail.com>
2024-07-05 10:59:12 +0900

    cart 기능 연결중

--- src/main/java/com/viaje/viaje/service/CartService.java
+++ src/main/java/com/viaje/viaje/service/CartService.java
@@ -3,15 +3,27 @@ package com.viaje.viaje.service;
 import com.viaje.viaje.model.Cart;
 import com.viaje.viaje.model.CartItems;
 import com.viaje.viaje.model.TravelPlans;
+import com.viaje.viaje.model.Users;
 import com.viaje.viaje.repository.CartItemsRepository;
+import com.viaje.viaje.repository.CartRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CartService {
-    @Autowired
+
     private CartItemsRepository cartItemsRepository;
+    private CartRepository cartRepository;
+    private UserService userService;
+
+    public CartService(CartItemsRepository cartItemsRepository, CartRepository cartRepository, UserService userService) {
+        this.cartItemsRepository = cartItemsRepository;
+        this.cartRepository = cartRepository;
+        this.userService = userService;
+    }
 
     @Transactional
     public void addToCart(Cart cart, TravelPlans plan, int quantity) {
@@ -29,4 +41,21 @@ public class CartService {
             cartItemsRepository.save(newItem);
         }
     }
+
+    public Cart getCart(String user_email) {
+        Users user = userService.findByEmail(user_email);
+        Cart cart = cartRepository.findById(user.getUserId())
+                .orElseGet(()->{
+                    Cart newCart = new Cart();
+                    newCart.setUsers(user);
+                    return cartRepository.save(newCart);
+                });
+        return cart;
+    }
+
+    public List<CartItems> findAllcartItmes(Cart cart) {
+        return cartItemsRepository.findByCart(cart);
+
+    }
+
 }
