5c3ea45789e77332c7a559966b75652f15197b88
aiden_Lee <isyouh77@gmail.com>
2024-07-10 10:53:28 +0900

    cart update

--- src/main/java/com/viaje/viaje/service/FileUploadUtil.java
+++ src/main/java/com/viaje/viaje/service/FileUploadUtil.java
@@ -2,6 +2,7 @@ package com.viaje.viaje.service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
