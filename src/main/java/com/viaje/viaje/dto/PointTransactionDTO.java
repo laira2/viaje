ffd67665f581ad221fb26480a1f79b349b4299e7
JeongChangYeol <devcy1029@gmail.com>
2024-07-10 12:53:34 +0900

    PointTransactionDTO 추가 내용이 없으면 실행 시 오류가 발생해서 추가합니다!

--- src/main/java/com/viaje/viaje/dto/PointTransactionDTO.java
+++ src/main/java/com/viaje/viaje/dto/PointTransactionDTO.java
@@ -8,5 +8,13 @@ import lombok.*;
 @NoArgsConstructor
 @Builder
 public class PointTransactionDTO {
+    private Long pointTransactionId;  // 트랜잭션 ID
+    private Long userId;  // 사용자 ID
+    private Integer chargeAmount;  // 충전 금액
+    private Integer chargePoint;  // 충전 포인트
+    private LocalDateTime createdAt;  // 생성 일시
+    private LocalDateTime updatedAt;  // 업데이트 일시
+    private String transactionStatus;  // 트랜잭션 상태
+    private String transactionType;  // 트랜잭션 타입
 
 }
