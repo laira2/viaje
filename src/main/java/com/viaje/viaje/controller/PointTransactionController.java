93da0847d36f9b0cad347745be50099072f65079
JeongChangYeol <devcy1029@gmail.com>
2024-07-10 16:01:03 +0900

    결제 서비스, 레파지토리, 컨트롤러 생성

--- src/main/java/com/viaje/viaje/controller/PointTransactionController.java
+++ src/main/java/com/viaje/viaje/controller/PointTransactionController.java
@@ -1,7 +1,29 @@
 package com.viaje.viaje.controller;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
+import com.viaje.viaje.dto.PointTransactionDTO;
+import com.viaje.viaje.service.PointTransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@Controller
 public class PointTransactionController {
+
+    private final PointTransactionService pointTransactionService;
+
+    @Autowired
+    public PointTransactionController(PointTransactionService pointTransactionService) {
+        this.pointTransactionService = pointTransactionService;
+    }
+
+    @PostMapping("/charge")
+    public String chargePoints(@RequestParam Long userId,
+                                                            @RequestParam Integer chargeAmount,
+                                                            @RequestParam Integer chargePoint) {
+        PointTransactionDTO transactionDTO = pointTransactionService.chargePoints(userId, chargeAmount, chargePoint);
+        return "redirect:/mypage";
+    }
 }
