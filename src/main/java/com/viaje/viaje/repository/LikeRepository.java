2543269913793c54b9c2ea561356732fa5bf5ab0
laira2 <laira241@gmail.com>
2024-07-23 14:55:24 +0900

    ã마이페이지 좋아요 기능 연결

--- src/main/java/com/viaje/viaje/repository/LikeRepository.java
+++ src/main/java/com/viaje/viaje/repository/LikeRepository.java
@@ -6,6 +6,7 @@ import com.viaje.viaje.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,4 +15,6 @@ public interface LikeRepository extends JpaRepository<Like, Long > {
     boolean existsByUsersAndTravelPlans(Users user, TravelPlans selectedPlan);
 
     Optional<Like> findByUsersAndTravelPlans(Users users, TravelPlans travelPlans);
+
+    List<Like> findAllByUsers(Users user);
 }
